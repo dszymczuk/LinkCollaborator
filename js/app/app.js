901b72a5a81b10ef2bd1e110d30940262407459f
Damian <damian.strony@gmail.com>
2014-07-20 19:06:30 +0200

    add model to inputs

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,13 @@
 var linkCollaborator = angular.module('linkCollaborator',[]);
 
 
-linkCollaborator.controller('AppController',function(){
+linkCollaborator.controller('AppController',function($scope){
 	console.log("Initial controller");
+
+    $scope.linkadd = {};
+
+
+    $scope.addLink = function(){
+        console.log($scope.linkadd);
+    };
 });
